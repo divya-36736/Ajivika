8662839bcbb599af3a529d267ced0f78488d5535
divya-36736 <23bec036@nith.ac.in>
2026-06-09 01:57:33 +0530

    Fix: Added /api to baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API = axios.create({
     // baseURL: 'http://localhost:3000/api', // Aapka backend URL
-    baseURL:'https://ajivika.onrender.com',
+    baseURL:'https://ajivika.onrender.com/api',
     withCredentials: true // Cookies (Token) send karne ke liye zaroori hai
 });
 
